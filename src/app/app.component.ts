9454fed0ba8353d573998077bf59eb875dd910eb
mohammed ashfaq <mdashfaqhussain510@gmail.com>
2023-11-06 16:56:35 +0530

    Angular-derivatives:Builtin-custom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lab-angular';
+  currentPage = 0;
+  items= [
+    {"id": 0,
+    "title": "My dog image",
+    "thumbnail": "https://loremflickr.com/150/150/dog",
+    "src": "https://loremflickr.com/1680/1050/dog"
+    },
+    {"id": 1,
+      "title": "My cat image",
+      "thumbnail": "https://loremflickr.com/150/150/cat",
+      "src": "https://loremflickr.com/1680/1050/cat"
+    },
+    {"id": 2,
+      "title": "My parrot image",
+      "thumbnail": "https://loremflickr.com/150/150/parrot",
+      "src": "https://loremflickr.com/1680/1050/parrot"
+    }
+  ]
+
+  onClickNext(event:Event){
+    event.preventDefault();
+    console.log(this.currentPage, this.items.length)
+
+    this.currentPage = this.currentPage + 1;
+
+
+
+
+  }
+
+  onClickPrev(event:Event){
+    event.preventDefault();
+    this.currentPage = this.currentPage - 1;
+    console.log(this.currentPage)
+  }
 }
